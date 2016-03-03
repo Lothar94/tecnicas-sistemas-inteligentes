abaea9df4cfe1a5caf9cae8d5ea50dc0de8a4fdf
David Charte <fdavidcl@outlook.com>
2016-03-03 11:33:33 +0100

    Initial files for catkin workspace

---
+++ 1-random-walk/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel/bin:/opt/ros/jade/bin:/home/fdavidcl/.rvm/gems/ruby-2.2.3/bin:/home/fdavidcl/.rvm/gems/ruby-2.2.3@global/bin:/home/fdavidcl/.rvm/rubies/ruby-2.2.3/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/opt/android-ndk:/opt/android-sdk/platform-tools:/opt/android-sdk/tools:/opt/COMODO:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/fdavidcl/.cabal/bin:/home/fdavidcl/.rvm/bin:/home/fdavidcl/.rvm/bin"
+export PKG_CONFIG_PATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export PWD="/media/datos/Documents/Universidad/4/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/build"
+export PYTHONPATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel/lib/python2.7/site-packages:/opt/ros/jade/lib/python2.7/site-packages:/opt/ros/jade/lib/python2.7/site-packages"
+export ROS_PACKAGE_PATH="/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/src:$ROS_PACKAGE_PATH"
