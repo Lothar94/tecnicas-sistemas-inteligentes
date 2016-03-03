474a766244144b6336c1e5d90381c2d559c38537
Lothar94 <lsp.lot94@gmail.com>
2016-03-03 12:58:14 +0100

    Borrando innecesarios

--- 1-random-walk/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel;/opt/ros/jade".split(';') if "/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/devel;/opt/ros/jade" != "" else []
--- 1-random-walk/build/random_walk/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;stage_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "random_walk"
-PROJECT_SPACE_DIR = "/home/fdavidcl/2/TSI/Practicas/tecnicas-sistemas-inteligentes/1-random-walk/install"
-PROJECT_VERSION = "0.0.0"
