853ab02604628765ee3b33fd0c656a9182123a0c
David Charte <fdavidcl@outlook.com>
2016-03-08 18:47:06 +0100

    Arregla conflictos varios

--- 2-random-walk/build/random_walk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/josecarlos/UGR/TSI/tecnicas-sistemas-inteligentes/1-random-walk/src/random_walk
-# Build directory: /home/josecarlos/UGR/TSI/tecnicas-sistemas-inteligentes/1-random-walk/build/random_walk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
