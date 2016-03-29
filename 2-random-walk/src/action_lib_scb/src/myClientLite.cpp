62d0085f2c29593defd35f8b18fefff9df582ecb
Lothar94 <lsp.lot94@gmail.com>
2016-03-29 16:46:09 +0200

    Añadido detector de colisiones

--- 2-random-walk/src/action_lib_scb/src/myClientLite.cpp
+++ 2-random-walk/src/action_lib_scb/src/myClientLite.cpp
@@ -4,9 +4,74 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+struct Tupla {double x; double y;};
+
+Tupla pos_inicial;
+double tiempo_inicial;
+bool cancelar = false;
+bool primera = true;
+bool primera_igual = true;
+
+double distancia(Tupla src, Tupla dst){
+    return sqrt((src.x - dst.x) * (src.x - dst.x) +
+                (src.y - dst.y) * (src.y - dst.y));
+}
+
 //Proporciona información mientras se intenta alcanzar la meta.
-void feedbackCBGoal0( const move_base_msgs::MoveBaseFeedbackConstPtr& feedback ){
-  //ROS_INFO("Hello... It's me...");
+void feedbackCBGoal0( const move_base_msgs::MoveBaseFeedback::ConstPtr& feedback ){
+  /*Tupla posicion;
+  double dist;
+  posicion.x = feedback->base_position.pose.position.x;
+  posicion.y = feedback->base_position.pose.position.y;
+
+  if(primera){
+    pos_inicial.x = posicion.x;
+    pos_inicial.y = posicion.y;
+    primera = false;
+  }
+  else{
+    dist = distancia(posicion, pos_inicial);
+    ROS_INFO("Distancia: %f", dist);
+    if(dist > 1.0){
+      cancelar = true;
+    }
+  }*/
+  double tiempo;
+  Tupla posicion;
+  static double tm = 0.0;
+  double tiempo_maximo = 3.0;
+  posicion.x = feedback->base_position.pose.position.x;
+  posicion.y = feedback->base_position.pose.position.y;
+
+  if( primera ){
+    pos_inicial.x = posicion.x;
+    pos_inicial.y = posicion.y;
+    primera = false;
+  }
+  else{
+    if( posicion.x == pos_inicial.x && posicion.y == pos_inicial.y ){
+      if( primera_igual ){
+        tiempo_inicial = ros::Time::now().toSec();
+        ROS_INFO("tiempo_inicial: %f", tiempo_inicial);
+        primera_igual = false;
+      }
+      else{
+        if( tm < tiempo_maximo ){
+          tiempo = ros::Time::now().toSec();
+          tm = tiempo-tiempo_inicial;
+          ROS_INFO("tiempo: %f", tm);
+        }
+        else{
+          cancelar = true;
+          ROS_INFO("cancelar");
+        }
+      }
+    }
+    else{
+      pos_inicial.x = posicion.x;
+      pos_inicial.y = posicion.y;
+    }
+  }
 }
 
 //Detecta si se alcanzó la meta o se canceló.
@@ -30,7 +95,6 @@ void activeCBGoal0(){
 }
 
 int main(int argc, char** argv){
-
   ros::init(argc,argv, "client_node");
 
   //crear el "action client"
@@ -65,6 +129,20 @@ int main(int argc, char** argv){
 
   ROS_INFO("Enviando el objetivo");
   ac.sendGoal(goal, &doneCBGoal0, &activeCBGoal0, &feedbackCBGoal0);
+
+  ros::Rate r(5); // 5 hz
+  tiempo_inicial = ros::Time::now().toSec();
+
+  while(!cancelar){
+    ros::spinOnce();
+    r.sleep();
+  }
+
+  if(cancelar){
+    ROS_INFO("El goal actual ha sido cancelado.");
+    ac.cancelGoal();
+    ac.waitForResult(ros::Duration(10.0));
+  }
 
   //Esperar al retorno de la acción
   ROS_INFO("Esperando al resultado  de la acción");
